8d2784b53c8e0ed3778e1a8da3594a39215830f3
alexr <belloa@hartwick.edu>
2020-03-26 09:56:52 -0400

    Started the coding

--- Stack_class.py
+++ Stack_class.py
@@ -1,1 +1,26 @@
+# Alex Bello
+# 3/26/2020
+# The stack and queue classes are stored here
+
+
+class Stack:
+    def __init__(self):
+        self.myList = []
+
+    def push(self, data):
+        self.myList.append(data)
+
+    def pop(self):
+        return self.myList[-1]
+
+
+class Queue:
+    def __init__(self):
+        self.mylist = []
+
+    def push(self, data):
+        self.mylist.add_end(data)
+
+    def pop(self):
+        return self.mylist.remove_head()
 
